e3e8649e53cc89bf6e037c361462b1a5c0bea817
psu6810110570 <chindanai.ac@gmail.com>
2026-02-22 20:31:27 +0700

    เพิ่มฟังก์ชันเริ่มเกมและเริ่มจับเวลาด้วย Clock

--- logic/game_engine.py
+++ logic/game_engine.py
@@ -4,6 +4,14 @@ import random
 class GameEngine:
     def __init__(self):
         self.score = 0
-        self.time_left = 60  # ให้เวลา 60 วินาที
+        self.time_left = 60
         self.is_playing = False
         self.timer_event = None
+
+    def start_game(self):
+        self.score = 0
+        self.time_left = 60
+        self.is_playing = True
+        
+        self.timer_event = Clock.schedule_interval(self.update_time, 1)
+        print("Game Started!")
