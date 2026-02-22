805af2423137c5e6c182a1fa83da7786b31e391d
psu6810110570 <chindanai.ac@gmail.com>
2026-02-22 20:36:59 +0700

    นับเวลาถอยหลัง

--- logic/game_engine.py
+++ logic/game_engine.py
@@ -15,3 +15,13 @@ class GameEngine:
         
         self.timer_event = Clock.schedule_interval(self.update_time, 1)
         print("Game Started!")
+
+    def update_time(self, dt):
+        if self.time_left > 0:
+            self.time_left -= 1
+            print(f"Time left: {self.time_left}")
+            
+            if self.time_left == 10:
+                print("10 seconds left!")
+        else:
+            self.game_over()
