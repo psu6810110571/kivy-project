16017d2c9f1390880cdbb63345b30b9dac05104e
psu6810110485 <6810110485@psu.ac.th>
2026-02-22 19:25:49 +0700

    add App class and ScreenManager to display ResultScreen

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import  Screen, ScreenManager
+from kivy.app import App
 
 class ResultScreen(Screen):
     def on_enter(self):
         print("เข้าสู่หน้าสรุปคะแนน")
+
+class MyGameApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(ResultScreen(name='result'))
+        return sm
+
+if __name__ == '__main__':
+    MyGameApp().run()
