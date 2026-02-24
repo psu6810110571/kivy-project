0e14b8a22d0c67e270f3a552b0716989825470f8
psu6810110485 <6810110485@psu.ac.th>
2026-02-24 15:19:52 +0700

    add grade and feedback calculation logic

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from kivy.app import App
 class ResultScreen(Screen):
     def on_enter(self):
         print("เข้าสู่หน้าสรุปคะแนน")
+        
+    def get_feedback(self,score,total_questions):   
+        percentage = (score / total_questions) * 100
+        if percentage == 100:
+            return "อัจฉริยะ! คุณได้คะแนนเต็ม!"
+        elif percentage >= 80:
+            return "ยอดเยี่ยมมาก! คุณมีความรู้แน่นปึ้ก" 
+        elif percentage >= 50:
+            return "ผ่านเกณฑ์! พยายามอีกนิดนะ"
+        else:
+            return "ไม่เป็นไรนะ ลองกลับไปทบทวนดูอีกที!"
+
 
 class MyGameApp(App):
     def build(self):
