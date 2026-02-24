1e8a7bb8af78913fc14aa1912bfb73528bf1d5ab
psu6810110485 <6810110485@psu.ac.th>
2026-02-24 16:04:34 +0700

    add THSarabunNew and fix display issues

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from kivy.uix.screenmanager import  Screen, ScreenManager
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.app import App
+from game_data import category_general
+
+class GameScreen(Screen):
+    def show_hint(self):
+        print("แสดงคำใบ้สำหรับคำถามนี้")
+        real_hint = category_general[0]["hint"]
+        self.ids.hint_label.text = f"Hint: {real_hint}"
 
 class ResultScreen(Screen):
     def on_enter(self):
         print("เข้าสู่หน้าสรุปคะแนน")
         
-    def get_feedback(self,score,total_questions):   
+    def get_feedback(self, score, total_questions):   
         percentage = (score / total_questions) * 100
         if percentage == 100:
             return "อัจฉริยะ! คุณได้คะแนนเต็ม!"
@@ -16,13 +23,12 @@ class ResultScreen(Screen):
         else:
             return "ไม่เป็นไรนะ ลองกลับไปทบทวนดูอีกที!"
 
-
 class MyGameApp(App):
     def build(self):
         sm = ScreenManager()
+        sm.add_widget(GameScreen(name='game'))
         sm.add_widget(ResultScreen(name='result'))
         return sm
     
-
 if __name__ == '__main__':
     MyGameApp().run()
